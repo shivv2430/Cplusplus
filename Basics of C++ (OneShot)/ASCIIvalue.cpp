e56332225a275e6a1758ef90fb8454804c767728
Shivani Pal <2430.shivanipal@gmail.com>
2026-01-12 00:34:36 +0530

    ASCIIvalue

---
+++ Basics of C++ (OneShot)/ASCIIvalue.cpp
@@ -0,0 +1,37 @@
+// ASCIIvalue
+//A=65
+//a=97
+//0=48
+#include <iostream>
+using namespace std;
+int main(){
+    char ch;
+    cin>>ch;
+    cout<< "Enter a number:"<<(char)ch<< endl;
+    
+}
+
+//Homework
+#include <iostream>
+using namespace std;
+int main(){
+    int a;
+    a=7/22*(3.14+2)*3/5;
+    cout<< a<< endl;
+    
+}
+//Homework
+#include <iostream>
+using namespace std;
+int main(){
+    float a=4+2%(-8);
+    cout<< a<< endl;
+}
+//Homework
+#include <iostream>
+using namespace std;
+int main(){
+    float d=2/7;
+    cout<< d<< endl;
+    
+}
