7f5b4afbcf4ca1dfa461a985dd7c26c55877f1bd
Shivani Pal <2430.shivanipal@gmail.com>
2026-01-12 00:03:53 +0530

    Typecasting,hierarchy,charDataType

---
+++ Basics of C++ (OneShot)/Hierarchy.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int i=2*3/4;   //BODMAS (from left to right)
+    cout<<i;
+}
---
+++ Basics of C++ (OneShot)/typecasting.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int x;
+    cin>>x;
+    cout<< "Enter a number:"<<x<< endl;
+    float y = (float)x;
+    cout<< y/2;
+}
