d8304bd3ae21d3147d5e89145302b68dc52cf8c5
shivv2430 <2430.shivanipal@gmail.com>
2026-01-11 21:59:02 +0530

    add gitignore and modulus program

--- Basics of C++ (OneShot)/Sum.cpp
+++ Basics of C++ (OneShot)/Sum.cpp
@@ -2,6 +2,7 @@
 using namespace std;
 int main (){
     float x,y,z;
+    cout<< "enter the value of x, y and z"<<endl;
     cin>>x>>y>>z;
     cout<< "first no.:"<<x<<endl<< "second no.:"<< y<< endl<<"Sum:"<< x+y<< endl;
 }
