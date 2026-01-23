2e8992bb9282df5a1f8f23401bb05ea2696de913
shivv2430 <2430.shivanipal@gmail.com>
2026-01-23 22:28:10 +0530

    CodeChef contest

---
+++ CodeChef Contest/Starters221/ProteinDiet.cpp
@@ -0,0 +1,15 @@
+// Problem Statement:-  https://www.codechef.com/problems/LMP1
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	int X,   Y;
+	cin >> X >> Y;
+	if (X>Y||X==Y){
+	    cout << "YES";
+	}
+	else
+	cout<< "NO";
+
+}
