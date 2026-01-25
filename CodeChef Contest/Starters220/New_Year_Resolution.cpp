78035bb6b45af1be84a52a58d5171dd2b1e53c15
shivv2430 <2430.shivanipal@gmail.com>
2026-01-25 22:31:39 +0530

    Contest_Questions

---
+++ CodeChef Contest/Starters220/New_Year_Resolution.cpp
@@ -0,0 +1,15 @@
+// Problem Statement : https://www.codechef.com/problems/NYRES
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int X;
+    cout<<"Enter number of pushups you did in a day : ";
+	cin>>X;
+	
+     cout<<"Total number of pushups you did in a January month of 2026 : ";
+	cout<<X*31;
+	
+
+}
