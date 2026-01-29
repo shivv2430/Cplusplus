90dee12564e504382dc40e29ed18e265e30d15eb
shivv2430 <2430.shivanipal@gmail.com>
2026-01-29 22:00:26 +0530

    Example2

---
+++ Chapter7_Recursion/Example2.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+// Function with no parameters and no return value
+void greet() {
+cout << "Welcome to C++ Programming!" << endl;
+// Function with parameters and a return value
+// int multiply(int a, int b) 
+// {
+// return a * b;
+// }
+}
+int main() {
+// Calling the greet function
+greet();
+// Calling the multiply function
+// int result = multiply(4, 5);
+// cout << "Multiplication result: " << result << endl;
+return 0;
+}
