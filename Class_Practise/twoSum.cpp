37b16b32af99146f08856a92d79df60ab3c22de6
shivv2430 <2430.shivanipal@gmail.com>
2026-03-13 23:43:27 +0530

    Leetcode question

--- Class_Practise/twoSum.cpp
+++ Class_Practise/twoSum.cpp
@@ -1,15 +1,19 @@
 //Leetcode Problem : https://leetcode.com/problems/two-sum/description/
-vector<int>two.sum(vector<int>nums&,int target)
-        {vector<int>ans;
-        for(int x=0:x<num;x++)
-            {for(int y=x+1;y<nums.size();y++)
-                {
-                    if(num[x]+num[y]==target)
-                    {
-                        ans.push_back(x);
-                        ans.push_back(y);
-                        return(ans);
-                    }
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> ans;
+
+        for(int x = 0; x < nums.size(); x++) {
+            for(int y = x + 1; y < nums.size(); y++) {
+                if(nums[x] + nums[y] == target) {
+                    ans.push_back(x);
+                    ans.push_back(y);
+                    return ans;
                 }
             }
         }
+
+        return ans;
+    }
+};
