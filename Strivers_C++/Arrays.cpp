444f778207eca1575a9d73de5a0cb987000fa177
shivv2430 <2430.shivanipal@gmail.com>
2026-03-18 22:16:41 +0530

    Arrays in c++

---
+++ Strivers_C++/Arrays.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main(){
+    // int arr[5];
+    // cin>>arr[0]>>arr[1]>>arr[2]>>arr[3]>>arr[4];
+    // arr[3]=16;
+    // arr[4]+=10;
+    // cout<<arr[2];
+    
+//2D Array
+string s = "Shivani";
+int len = s.size();
+cout<<s[1];
+cout<<s[len-1];
+   
+}
