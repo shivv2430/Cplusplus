e8c3e4638f9fbae118c570b9f7faaa54f88899d3
shivv2430 <2430.shivanipal@gmail.com>
2026-03-20 00:20:33 +0530

    functions by striver

---
+++ Strivers_C++/Function.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+    // function_type function-Name(parameter)
+    void PrintName(string name){
+        cout<<"Hello"<<name;
+    }
+    int main (){
+        string name;
+        cin>>name;
+        PrintName(name);
+
+    }
