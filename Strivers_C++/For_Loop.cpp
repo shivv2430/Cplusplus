4e0e1bb7980951e77b08241cb14e8351a1cf94c6
shivv2430 <2430.shivanipal@gmail.com>
2026-03-19 23:41:27 +0530

    for-Loops by striver

--- Strivers_C++/For_Loop.cpp
+++ Strivers_C++/For_Loop.cpp
@@ -11,9 +11,23 @@ int main (){
     //     cout<<"Shivani "<<j<<endl;
     // }
 
-    int k;
-    for(k=1;k<25;k+=5){
-        cout<<"Shivani "<<k<<endl;
-    }
+    // int k;
+    // for(k=1;k<25;k+=5){
+    //     cout<<"Shivani "<<k<<endl;
+    // }
     
+//While
+    // int i = 1;
+    // while(i<=25){
+    //     cout<<"shivani"<<i<<endl;
+    //     i=i+1;
+    // }
+
+//Do-While
+    int i =2;
+    do{
+        cout<<"Hello"<<i<<endl;
+        i=i+1;
+    }
+    while (i<25);
 }
