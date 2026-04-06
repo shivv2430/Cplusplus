a6af4c593049fe61c06437c8421c8dbc57d3f569
shivv2430 <2430.shivanipal@gmail.com>
2026-04-07 00:21:27 +0530

    codechef contest

---
+++ CodeChef Contest/Starters229/Vacations.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main() {
+  // N => no. of days
+  // E => Excitement
+
+  int N, E;
+  cout << "Enter the no.of days left for vacations : ";
+  cin >> N;
+  cout << "Enter your excitement on day 1 : ";
+  cin >> E;
+  cout << "Your total excitement will be : ";
+  cout << E + (N - 1) << endl;
+}
