f0c6f3628ed5fa64437667b5af5db4a1b7ea6b64
shivv2430 <2430.shivanipal@gmail.com>
2026-04-08 18:34:17 +0530

    codechef contest

---
+++ CodeChef Contest/Starters230/Pass_Exams.cpp
@@ -0,0 +1,27 @@
+// Problem-statement: https://www.codechef.com/problems/PASSEXAMS
+#include <iostream>
+using namespace std;
+
+int main() {
+  int T;
+  cin >> T;
+
+  for (int i = 0; i < T; i++) {
+    int X, Y, Z;
+    cin >> X >> Y >> Z;
+
+    int count = 0;
+
+    if (X >= 50)
+      count++;
+    if (Y >= 50)
+      count++;
+    if (Z >= 50)
+      count++;
+
+    if (count >= 2)
+      cout << "Yes" << endl;
+    else
+      cout << "No" << endl;
+  }
+}
