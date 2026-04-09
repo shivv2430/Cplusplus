71d605661e684a7a23cba7e6167edc1df3414770
shivv2430 <2430.shivanipal@gmail.com>
2026-04-09 19:51:06 +0530

    codechef problem

---
+++ CodeChef Contest/Starters232/Vacations_Clothes.cpp
@@ -0,0 +1,12 @@
+// Problem Statement : https://www.codechef.com/problems/VACCLOTH
+#include <iostream>
+using namespace std;
+int main() {
+  int N;
+  cin >> N;
+
+  if (N < 8) {
+    cout << N;
+  } else
+    cout << 7;
+}
