9197d1ad31c6e4552cdd1ad6e62353b68b49ff65
shivv2430 <2430.shivanipal@gmail.com>
2026-04-15 17:31:43 +0530

    class-work

---
+++ Class_Practise/Sll-Create&traverse.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+using namespace std;
+
+class node {
+public:
+  int info;
+  node *link;
+
+  node(int value) // constructor
+  {
+    info = value;
+    link = NULL;
+  }
+};
+
+// Function to create a node
+node *create_node(int value) {
+  node *ptr;
+  ptr = new node(value);
+  return ptr;
+}
+
+// Function to create singly linked list
+node *create_sll() {
+  int value;
+  cin >> value;
+
+  node *ptr, *nptr, *start;
+
+  ptr = create_node(value);
+  start = ptr;
+
+  int ch;
+  cin >> ch; // ch = 1 (yes), 0 (no)
+
+  while (ch == 1) {
+    cin >> value;
+    nptr = create_node(value);
+
+    ptr->link = nptr;
+    ptr = nptr;
+
+    cin >> ch;
+  }
+
+  return start;
+}
+
+// Function to traverse and display list
+void traverse(node *temp) {
+  while (temp != NULL) {
+    cout << temp->info << " ";
+    temp = temp->link;
+  }
+}
+
+// Main function
+int main() {
+  node *start;
+  start = create_sll();
+
+  traverse(start);
+
+  return 0;
+}
