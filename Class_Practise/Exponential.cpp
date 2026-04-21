d2788f62f73a48e8ba3ac75f53d542dc26abb132
shivv2430 <2430.shivanipal@gmail.com>
2026-04-21 13:49:47 +0530

    exponential

---
+++ Class_Practise/Exponential.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+using namespace std;
+
+class node {
+public:
+  int coeff, expo;
+  node *link;
+
+  node(int a, int b) {
+    coeff = a;
+    expo = b;
+    link = NULL;
+  }
+};
+
+node *Create_node(int c, int e) { return new node(c, e); }
+
+node *Create_poly() {
+  int a, b;
+  cin >> a >> b;
+
+  node *ptr, *start, *nptr;
+  ptr = Create_node(a, b);
+  start = ptr;
+
+  int ch;
+  cin >> ch;
+
+  while (ch == 1) {
+    cin >> a >> b;
+    nptr = Create_node(a, b);
+    ptr->link = nptr;
+    ptr = nptr;
+    cin >> ch;
+  }
+  return start;
+}
+
+void traverse(node *start) {
+  node *temp = start;
+
+  while (temp != NULL) {
+    cout << temp->coeff << "X^" << temp->expo;
+    if (temp->link != NULL)
+      cout << " + ";
+    temp = temp->link;
+  }
+}
+
+int main() {
+  node *start;
+  start = Create_poly();
+  traverse(start);
+}
