cf2068c815e5ac057a0ab17ba40de53812600b36
shivv2430 <2430.shivanipal@gmail.com>
2026-04-24 13:33:14 +0530

    CodeChef

---
+++ CodeChef Contest/Starters235/Red-fee.cpp
@@ -0,0 +1,13 @@
+// Problem statement : https://www.codechef.com/problems/P1235
+
+#include <iostream>
+using namespace std;
+int main() {
+  int X, Y;
+  cin >> X >> Y;
+
+  if (Y <= X) {
+    cout << "0" << endl;
+  } else
+    cout << "100" << endl;
+}
