ee8a8fc687a48317d7ff9fd6b3e98b49fb0c35c3
shivv2430 <2430.shivanipal@gmail.com>
2026-05-19 23:41:02 +0530

    practise

---
+++ Lab_Practical/Sem-2/p-3.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int n;
+
+  // Input size of array
+  cout << "Enter number of elements: ";
+  cin >> n;
+
+  int arr[n];
+
+  // Input array elements
+  cout << "Enter array elements:\n";
+  for (int i = 0; i < n; i++) {
+    cin >> arr[i];
+  }
+
+  // Bubble Sort
+  for (int i = 0; i < n - 1; i++) {
+    for (int j = 0; j < n - i - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        // Swap elements
+        int temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+      }
+    }
+  }
+
+  // Display sorted array
+  cout << "Sorted array:\n";
+  for (int i = 0; i < n; i++) {
+    cout << arr[i] << " ";
+  }
+
+  return 0;
+}
