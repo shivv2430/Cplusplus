c10a2e5d631e4fbca08f5c9937c22e40dc3c998b
shivv2430 <2430.shivanipal@gmail.com>
2026-06-03 23:36:11 +0530

    codechef contest

---
+++ CodeChef Contest/Startrers241/RunChase.cpp
@@ -0,0 +1,13 @@
+//problem-statement: https://www.codechef.com/problems/RUNCHASE
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int N;
+    cin >> N;
+
+    cout << (N / 20) + 1 << endl;
+
+    return 0;
+}
