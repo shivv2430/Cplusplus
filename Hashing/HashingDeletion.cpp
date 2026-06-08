d4a2486c68d216e1c04f06a376453064d1149ec3
shivv2430 <2430.shivanipal@gmail.com>
2026-06-08 22:22:08 +0530

    HashingDeletion

---
+++ Hashing/HashingDeletion.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+using namespace std;
+int main() {
+  int size;
+  cin >> size;
+
+  int HT[size];
+  for (int i = 0; i < size; i++) {
+    HT[i] = -1;
+  }
+
+  int n;
+  cin >> n;
+
+  for (int i = 0; i < n; i++) {
+    int key;
+    cin >> key;
+
+    int index = key % size;
+
+    while (HT[index] != -1) {
+      index = (index + 1) % size;
+    }
+
+    HT[index] = key;
+  }
+
+  for (int i = 0; i < size; i++) {
+    cout << HT[i] << " ";
+  }
+
+  cout << "Enter the key to be deleted: ";
+  int key;
+  cin >> key;
+
+  int index = key % size;
+
+  while (HT[index] != key) {
+    index = (index + 1) % size;
+  }
+
+  HT[index] = -1;
+
+  for (int i = 0; i < size; i++) {
+    cout << HT[i] << " ";
+  }
+}
