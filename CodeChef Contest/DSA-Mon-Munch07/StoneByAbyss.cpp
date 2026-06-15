c80d947a63fe718dfe180b74d011e852ec6eddf5
shivv2430 <2430.shivanipal@gmail.com>
2026-06-15 20:50:54 +0530

    CodeChef

---
+++ CodeChef Contest/DSA-Mon-Munch07/StoneByAbyss.cpp
@@ -0,0 +1,6 @@
+// Problem-Statement:https://www.codechef.com/DSAMONDAY007/problems/STONEABYSS
+
+#include <string>
+using namespace std;
+
+string stoneAbbys(int N) { return (N % 3 == 0) ? "Bob" : "Alice"; }
