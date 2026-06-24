7a095abc26a8767b6658a3997fed343bc4a608e3
shivv2430 <2430.shivanipal@gmail.com>
2026-06-24 22:57:38 +0530

    update file

--- Strings/Example5.cpp
+++ Strings/Example5.cpp
@@ -1,15 +1,14 @@
 // Example: C++ program to compare strings using compare() function
 #include <iostream>
-#include<string>
+#include <string>
 using namespace std;
-int main()
-{
-string str1 = "Welcome";
-string str2 = "Home";
-if ((str1.compare(str2)) == 0)
-cout << "String Matched" << endl;
-else
-cout << "String Not Matched" << endl;
-return 0;}
+int main() {
+  string str1 = "Welcome";
+  string str2 = "Home";
+  if ((str1.compare(str2)) == 0)
+    cout << "String Matched" << endl;
+  else
+    cout << "String Not Matched" << endl;
+}
 // OUTPUT
 // String Not Matched
