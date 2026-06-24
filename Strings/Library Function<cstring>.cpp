2665b3ffa19cf9f61fc8d38b5cb75a4c0ea8d3c9
shivv2430 <2430.shivanipal@gmail.com>
2026-06-24 22:58:24 +0530

    correct the file issue

--- Strings/Library Function<cstring>.cpp
+++ Strings/Library Function<cstring>.cpp
@@ -1,29 +1,29 @@
 // strlen(fname)     for name
 // strcpy(fname)     for copy
-//strcmp(fname,lname)      for compare
-//strcat(fname,lname)
-#include<iostream>
-#include<cstring>
+// strcmp(fname,lname)      for compare
+// strcat(fname,lname)
+#include <cstring>
+#include <iostream>
 using namespace std;
-int main()
-{
-char fname[20];
-char lname[20];
-char fullname[50];
-cout<<"Enter your first name: ";
-cin>>fname;
-cout<<"Enter your last name: ";
-cin>>lname;
-cout<<"Hi " <<fname<<"! Your first name has "<<strlen(fname)<<"characters."<<endl<<"And your last name has "<<strlen(lname)<<"characters."<<endl;
-strcpy(fullname,fname);
-strcat(fullname, " ");
-strcat(fullname,lname);
-cout<<"Your Full name is: "<<fullname<<endl;
-if((strcmp(fname,lname))==0)
-cout<<"Both strings are equal."<<endl;
-else
-cout<<"Strings are not equal."<<endl;
-return 0;
+int main() {
+  char fname[20];
+  char lname[20];
+  char fullname[50];
+  cout << "Enter your first name: ";
+  cin >> fname;
+  cout << "Enter your last name: ";
+  cin >> lname;
+  cout << "Hi " << fname << "! Your first name has " << strlen(fname)
+       << "characters." << endl
+       << "And your last name has " << strlen(lname) << "characters." << endl;
+  strcpy(fullname, fname);
+  strcat(fullname, " ");
+  strcat(fullname, lname);
+  cout << "Your Full name is: " << fullname << endl;
+  if ((strcmp(fname, lname)) == 0)
+    cout << "Both strings are equal." << endl;
+  else
+    cout << "Strings are not equal." << endl;
 }
 
 // OUTPUT
--- Strings/Library_Function<string>.cpp
+++ Strings/Library_Function<string>.cpp
@@ -3,9 +3,9 @@
 
 using namespace std;
 int main() {
-  string str1 = {"hello"};
-  string str2{"Joy"};
-  string str3{str2};
+  string str1 = "hello";
+  string str2 = "Joy";
+  string str3 = str2;
   string str4(5, 'a'); // Displaying 'a' 5 times
   cout << str1 << endl << str2 << endl << str3 << endl << str4;
   return 0;
