b1a411f21fe57b42725042c8b0414824b282a141
shivv2430 <2430.shivanipal@gmail.com>
2026-06-26 23:53:49 +0530

    floyd patterns

--- Chapter4_Patterns/floyd.cpp
+++ Chapter4_Patterns/floyd.cpp
@@ -1,16 +1,17 @@
 #include <iostream>
 using namespace std;
 int main() {
-    int n;
-    cout<<" Enter a no. :";
-    cin>>n;
-    int a=1;
-    for(int i=1;i<=n;i++){{
-        for (int j=1;j<=i;j++){
-            cout<< a<<" ";
-            a++;
-        }
+  int n;
+  cout << " Enter a no. :" << endl;
+  cin >> n;
+  int a = 1;
+  for (int i = 1; i <= n; i++) {
+    {
+      for (int j = 1; j <= i; j++) {
+        cout << a << " ";
+        a++;
+      }
     }
-     cout<<endl;
-    }
+    cout << endl;
+  }
 }
