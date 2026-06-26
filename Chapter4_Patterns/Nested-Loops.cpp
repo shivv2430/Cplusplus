1fe9bac93044a6614ee67194a1209b0d270b5207
shivv2430 <2430.shivanipal@gmail.com>
2026-06-26 23:55:06 +0530

    nested-Loop patterns

--- Chapter4_Patterns/Nested-Loops.cpp
+++ Chapter4_Patterns/Nested-Loops.cpp
@@ -1,10 +1,10 @@
 #include <iostream>
 using namespace std;
 int main() {
-    int m;
-    cout<< "Enter no. of rows";
-    cin>>m;
-    for(int i=1;i<=m;i++){
-        cout<< "****"<< endl;
-    }
+  int m;
+  cout << "Enter no. of rows" << endl;
+  cin >> m;
+  for (int i = 1; i <= m; i++) {
+    cout << "****" << endl;
+  }
 }
