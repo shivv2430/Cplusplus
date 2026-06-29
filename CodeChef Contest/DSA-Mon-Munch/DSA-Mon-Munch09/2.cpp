7c76e3f6672e73e757bb859017c6f04ce37c8517
shivv2430 <2430.shivanipal@gmail.com>
2026-06-30 00:00:15 +0530

    codechef

---
+++ CodeChef Contest/DSA-Mon-Munch/DSA-Mon-Munch09/2.cpp
@@ -0,0 +1,33 @@
+#include <algorithm>
+#include <climits>
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+  int N;
+  cin >> N;
+
+  vector<long long> arr(N);
+  for (int i = 0; i < N; i++) {
+    cin >> arr[i];
+  }
+
+  vector<long long> leaders;
+  long long maxRight = LLONG_MIN;
+
+  for (int i = N - 1; i >= 0; i--) {
+    if (arr[i] > maxRight) {
+      leaders.push_back(arr[i]);
+      maxRight = arr[i];
+    }
+  }
+
+  reverse(leaders.begin(), leaders.end());
+
+  for (long long x : leaders) {
+    cout << x << " ";
+  }
+
+  return 0;
+}
