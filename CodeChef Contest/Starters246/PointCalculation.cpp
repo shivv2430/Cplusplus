c52c49217aca5b3498d13af6b15fed4fcfa142bb
shivv2430 <2430.shivanipal@gmail.com>
2026-07-10 21:12:35 +0530

    starters 246 -1

---
+++ CodeChef Contest/Starters246/PointCalculation.cpp
@@ -0,0 +1,11 @@
+// Problem_Statement : https://www.codechef.com/problems/POINTCAL
+
+#include <iostream>
+using namespace std;
+
+int main() {
+  int A, B, C;
+  cin >> A >> B >> C;
+
+  cout << ((A * 3) + (B * 1) + (0 * C)) << endl;
+}
