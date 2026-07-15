6c77f80d00243d65c00ea1675292b06646262d4f
shivv2430 <2430.shivanipal@gmail.com>
2026-07-15 18:20:18 +0530

    monday-munch11

---
+++ CodeChef Contest/DSA-Mon-Munch/DSA-Mon-Munch11/Monthly-earning.cpp
@@ -0,0 +1,10 @@
+// Problem-statement : https://www.codechef.com/problems/SALARY1
+
+#include <iostream>
+using namespace std;
+
+int main() {
+  int S, B, R;
+  cin >> S >> B >> R;
+  cout << S + (B * R);
+}
