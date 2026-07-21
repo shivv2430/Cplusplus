bf30ae2b4132a75886820476ed28985bd433fa6f
shivv2430 <2430.shivanipal@gmail.com>
2026-07-21 20:01:09 +0530

    codechef contest

---
+++ CodeChef Contest/DSA-Mon-Munch/DSA-Mon-Munch11/Pen-Shortage.cpp
@@ -0,0 +1,11 @@
+
+// Problem-statement : https://www.codechef.com/problems/ACT01
+#include <iostream>
+using namespace std;
+
+int main() {
+  int N, P;
+  cin >> N >> P;
+
+  cout << N - P << endl;
+}
