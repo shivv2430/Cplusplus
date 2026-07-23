9d2260a655aced439ce2159762e543e778febfdc
shivv2430 <2430.shivanipal@gmail.com>
2026-07-23 19:26:08 +0530

    leetcode valid parenthesis

---
+++ 3rd-Semester/Stack/practical-file_work/Leetcode_Valid_Parenthesis.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+class Solution {
+public:
+  bool isValid(string s) {
+    vector<char> st;
+
+    for (int i = 0; i < s.length(); i++) {
+      char ch = s[i];
+
+      if (ch == '(' || ch == '{' || ch == '[') {
+        st.push_back(ch);
+      } else {
+        if (st.empty()) {
+          return false;
+        }
+
+        if ((ch == ')' && st.back() == '(') ||
+            (ch == '}' && st.back() == '{') ||
+            (ch == ']' && st.back() == '[')) {
+
+          st.pop_back();
+        } else {
+          return false;
+        }
+      }
+    }
+
+    return st.empty();
+  }
+};
