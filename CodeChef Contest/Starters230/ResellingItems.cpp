c159e9f74e4213d526db25c04f354f2c27cda192
shivv2430 <2430.shivanipal@gmail.com>
2026-08-01 11:53:45 +0530

    reset file

--- CodeChef Contest/Starters230/ResellingItems.cpp
+++ CodeChef Contest/Starters230/ResellingItems.cpp
@@ -3,21 +3,16 @@
 #include <vector>
 #include <algorithm>
 using namespace std;
-
 int main() {
     int T;
     cin >> T;
-
     while (T--) {
         int N, K;
         cin >> N >> K;
-
         vector<int> A(N);
-
         for (int i = 0; i < N; i++) {
             cin >> A[i];
         }
-
         sort(A.begin(), A.end(), greater<int>());
 
         long long profit = 0;
@@ -35,9 +30,7 @@ int main() {
                 }
             }
         }
-
         cout << profit << "\n";
     }
-
     return 0;
 }
