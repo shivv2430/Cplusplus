19df024b9c2da073da50d7ef88ad65835a818f31
shivv2430 <2430.shivanipal@gmail.com>
2026-08-05 20:53:16 +0530

    Starters250

---
+++ CodeChef Contest/Starters250/PPT.cpp
@@ -0,0 +1,12 @@
+// Problem-Statement : https://www.codechef.com/START250D/problems/PPT
+#include <iostream>
+using namespace std;
+
+int main() {
+  int N;
+  cin >> N;
+
+  cout << 20 - N << endl;
+
+  return 0;
+}
