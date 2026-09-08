6151651dcd6655cbd451271239e977aa8ea25743
shivv2430 <2430.shivanipal@gmail.com>
2026-09-08 23:11:24 +0530

    dequeue function By_circulararray

--- 3rd-Semester/DEQUE/By_circulararray.cpp
+++ 3rd-Semester/DEQUE/By_circulararray.cpp
@@ -3,28 +3,61 @@ using namespace std;
 
 #define N 5
 
+int dequeArr[N];
 int front = -1;
 int rear = -1;
 
 void enqueueFront(int x) {
   if (front == -1 && rear == -1) {
     front = rear = 0;
+    dequeArr[front] = x;
   } else if ((rear + 1) % N == front) {
     cout << "Queue is full, Overflow" << endl;
   } else if (front == 0) {
     front = N - 1;
+    dequeArr[front] = x;
   } else {
     front = front - 1;
+    dequeArr[front] = x;
   }
 }
 
 void enqueueRear(int x) {
   if (front == -1 && rear == -1) {
     front = rear = 0;
+    dequeArr[rear] = x;
   } else if ((rear + 1) % N == front) {
     cout << "Queue is full, Overflow" << endl;
   } else {
     rear = (rear + 1) % N;
+    dequeArr[rear] = x;
+  }
+}
+
+void dequeueFront() {
+  if (front == -1 && rear == -1) {
+    cout << "No element in deque, Underflow" << endl;
+  } else if (front == rear) {
+    cout << "Dequeued element is " << dequeArr[front] << endl;
+    front = rear = -1;
+  } else {
+    cout << "Dequeued element is " << dequeArr[front] << endl;
+    front = (front + 1) % N;
+  }
+}
+
+void dequeueRear() {
+  if (front == -1 && rear == -1) {
+    cout << "No element in deque, Underflow" << endl;
+  } else if (front == rear) {
+    cout << "Dequeued element is " << dequeArr[rear] << endl;
+    front = rear = -1;
+  } else if (rear == 0) {
+    cout << "Dequeued element is " << dequeArr[rear] << endl;
+    rear = N - 1;
+  } else {
+    cout << "Dequeued element is " << dequeArr[rear] << endl;
+    rear = rear - 1;
   }
 }
 
@@ -33,6 +66,16 @@ void display() {
     cout << "Deque is empty" << endl;
     return;
   }
+
+  int i = front;
+  cout << "Elements of deque are: ";
+
+  while (i != rear) {
+    cout << dequeArr[i] << " ";
+    i = (i + 1) % N;
+  }
+
+  cout << dequeArr[rear] << endl;
 }
 
 int main() {
@@ -40,6 +83,10 @@ int main() {
   enqueueFront(20);
   enqueueRear(30);
   enqueueRear(40);
+
+  display();
+
+  dequeueRear();
 
   display();
 
