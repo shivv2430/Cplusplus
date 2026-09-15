2f3c6ec6427001090ccffe3af6f1056a02dd3dbe
shivv2430 <2430.shivanipal@gmail.com>
2026-09-15 22:17:22 +0530

    C++ Bottom-Up Approach(sum(n))

--- 3rd-Semester/Recursion/BottomUp_Approach.cpp
+++ 3rd-Semester/Recursion/BottomUp_Approach.cpp
@@ -23,3 +23,29 @@ int main() {
 
   return 0;
 }
+
+//=======sum of n numbers==============
+#include <iostream>
+using namespace std;
+
+int sum(int n) {
+  int result = 0;
+
+  for (int i = 1; i <= n; i++) {
+    result = result + i;
+  }
+
+  return result;
+}
+
+// int main() {
+//   int n;
+
+//   cout << "Enter the number: ";
+//   cin >> n;
+
+//   cout << "Sum of " << n << " numbers is: ";
+//   cout << sum(n) << endl;
+
+//   return 0;
+// }
